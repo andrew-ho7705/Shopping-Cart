d7f640464f3628ebde25000e926a7fa6a92a2ae4
Andrew Ho <amh7705@psu.edu>
2022-09-09 18:57:18 -0400

    Update nav. bar and add shopping cart

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,42 @@
-import { Container, Nav, Navbar as NavbarBs } from "react-bootstrap"
+import { Button, Container, Nav, Navbar as NavbarBs } from "react-bootstrap"
 import { NavLink } from "react-router-dom"
+import { FaShoppingCart } from "react-icons/fa"
 
 export function Navbar() {
     return (
-        <NavbarBs className="bg-white shadow-sm mb-3">
+        <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
             <Container>
-                <Nav>
-                    <Nav.Link to="/" as ={NavLink}>
+                <Nav className="me-auto">
+                    <Nav.Link to="/" as={NavLink}>
                         Home
                     </Nav.Link>
 
-                    <Nav.Link to="/about" as ={NavLink}>
+                    <Nav.Link to="/about" as={NavLink}>
                         About
                     </Nav.Link>
 
-                    <Nav.Link to="/store" as ={NavLink}>
+                    <Nav.Link to="/store" as={NavLink}>
                         Store
                     </Nav.Link>
                 </Nav>
+                <Button
+                    style={{ width: "3rem", height: "3rem", position: "relative" }}
+                    variant="outline-primary"
+                    className="rounded-circle">
+                    <FaShoppingCart />
+                    <div
+                        className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+                        style={{
+                            color: "white",
+                            width: "1.5rem",
+                            height: "1.5rem",
+                            position: "absolute",
+                            bottom: 0,
+                            right: 0,
+                            transform: "translate(25%, 25%)"
+                        }}
+                    ></div>
+                </Button>
             </Container>
         </NavbarBs>
     )
